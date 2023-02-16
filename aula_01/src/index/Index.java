179ee2f00eba0910b10cb0ccc4b3ec9cd418cc0c
GUSTAVO GONCALVES PIRES <uniegupires>
2023-02-16 20:12:34 -0300

    add index file to select which exercise should be run

---
+++ aula_01/src/index/Index.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package index;
+
+import exercicio_01.Exercicio_01;
+import exercicio_02.Exercicio_02;
+import exercicio_03.Exercicio_03;
+import exercicio_04.Exercicio_04;
+import java.util.Scanner;
+
+/**
+ *
+ * @author uniegupires
+ */
+public class Index {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Escolha o exercicio a ser executado (1 a 4):");
+        int exe = input.nextInt();
+        
+        switch(exe) {
+            case 1:
+                printExe(exe);
+                Exercicio_01.main(null);
+                break;
+            case 2:
+                printExe(exe);
+                Exercicio_02.main(null);
+                break;
+            case 3:
+                printExe(exe);
+                Exercicio_03.main(null);
+                break;
+            case 4:
+                printExe(exe);
+                Exercicio_04.main(null);
+                break;
+            default:
+                System.out.println("Escolha invalida. Por favor escolha um numero entre 1 e 4");
+                break;
+        }
+    }
+    
+    private static void printExe(int num) {
+        System.out.printf("==== EXECUTANDO EXERCICIO %d ====\n", num);
+    }
+    
+}
